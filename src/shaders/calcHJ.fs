983dc5677b92aa09187430b642554f470faced7b
Emanuel <emanueltrabes@gmail.com>
2020-12-03 16:07:56 -0300

    cambiando la implementacion en base a las ultimas reflecciones sobre el asunto

--- src/shaders/calcHJ.fs
+++ src/shaders/calcHJ.fs
@@ -3,25 +3,33 @@ layout(location = 0) out float f_residual;
 layout(location = 1) out vec3 f_tra;
 layout(location = 2) out vec3 f_rot;
 
-in vec3 v_pframe;
 in vec3 v_pkeyframe;
 
 uniform sampler2D keyframe;
 uniform sampler2D frame;
 uniform sampler2D frameDer;
 
+uniform mat4 framePose;
+
+uniform mat3 invK;
 uniform mat3 K;
 uniform float dx;
 uniform float dy;
 
 void main()
 {
+    //calculo u y depth con respecto al keyframe
+    //vec2 ukf = vec2(gl_FragCoord.x,1.0/dy-gl_FragCoord.y);
+    //dkf = gl_FragCoord.z/gl_FragCoord.w;
+
+    vec3 pframe = (framePose * vec4(v_pkeyframe,1.0)).xyz;
+
     //me esta causando nands en los frame buffers esto, no se porque
     //if(v_pframe.z <= 0.0 || v_pkeyframe.z <= 0.0)
     //  discard;
 
-    vec2 uframe = (K*(v_pframe.xyz/v_pframe.z)).xy;
-    vec2 ukeyframe = (K*(v_pkeyframe.xyz/v_pkeyframe.z)).xy;
+    vec2 uframe = (K*(pframe.xyz/pframe.z)).xy;
+    vec2 ukeyframe = vec2(gl_FragCoord.x,1.0/dy-gl_FragCoord.y);
 
     vec2 uframeTexCoord = vec2(uframe.x*dx, 1.0-uframe.y*dy);
     vec2 ukeyframeTexCoord = vec2(ukeyframe.x*dx, 1.0-ukeyframe.y*dy);
@@ -29,7 +37,6 @@ void main()
     float f_pixel = texture(frame, uframeTexCoord).x;
     vec2 f_der = texture(frameDer, uframeTexCoord).xy;
     float kf_pixel = texture(keyframe, ukeyframeTexCoord).x;
-    vec3 pframe = v_pframe;
 
     //este no me causa nands, muy extraño!
     if(kf_pixel < 0.0 || f_pixel < 0.0)
--- src/shaders/calcHJ.vs
+++ src/shaders/calcHJ.vs
@@ -1,26 +1,18 @@
 #version 330 core
 layout (location = 0) in vec3 p;
 
-out vec3 v_pframe;
 out vec3 v_pkeyframe;
 
-uniform mat4 framePose;
 uniform mat4 keyframePose;
 uniform mat4 projection;
 uniform mat4 opencv2opengl;
 
 void main()
 {
-    //vec2 u = vec2(p.x*width, p.y*height);
-    //float depth = 1.0/p.z;
-    //vec4 pworld = vec4(invK*(vec3(u,1.0)*depth), 1.0f);
-
     vec4 pworld = vec4(p, 1.0);
-    vec4 pframe = framePose * pworld;
     vec4 pkeyframe = keyframePose * pworld;
 
-    gl_Position = projection * opencv2opengl * pframe;
+    gl_Position = projection * opencv2opengl * pkeyframe;
 
-    v_pframe = pframe.xyz;
     v_pkeyframe = pkeyframe.xyz;
 }
